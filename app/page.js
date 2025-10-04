ea2da079d328ec452b25aed3e88be6c51a7d97dc
LucasHenrique569 <cs206838@gmail.com>
2025-10-04 17:42:18 -0400

    criando o header e definindo a estrutura inicial do site

--- app/page.js
+++ app/page.js
@@ -1,15 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import './globals.css'
+import './homePage.css'
+import { Castoro_Titling } from "next/font/google"
+
+const castoroTitlingSettings = Castoro_Titling({
+  subsets: ['latin'],
+  weight: ['400'],
+})
 
 export default function Home() {
   return (
-    <div >
-      <main >
-       <a>ola</a>
+    <div>
+      <header>
+        <h1 className={`${castoroTitlingSettings.className} title`}>Murilo <br></br> Yukio</h1>
+        <p className={`${castoroTitlingSettings.className} title`}>°</p>
+        <h1 className={`${castoroTitlingSettings.className} title`}>Desenvolvedor</h1>
+      </header>
+
+      <main>
+        <h1>Bem vindos ao Portfólio do MuMu</h1>
       </main>
-      <footer>
-      
-      </footer>
     </div>
   );
 }
